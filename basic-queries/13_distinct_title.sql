1c61676faabb9889e33e07a9f13ca7cdcf738c8d
Lesley Solorzano <lsolorzano@appdev.yearup.org>
2025-06-03 15:26:20 -0500

    Completed exercises 7-15.

---
+++ basic-queries/13_distinct_title.sql
@@ -0,0 +1,4 @@
+SELECT
+    DISTINCT E.Title
+FROM
+	Employees E
---
+++ basic-queries/14_salary_between_2000_2500.sql
@@ -0,0 +1,9 @@
+SELECT
+	E.FirstName,
+    E.LastName,
+    CONCAT(E.FirstName, " ", E.LastName) as FullName,
+    E.Salary
+FROM
+	Employees E
+WHERE
+	E.Salary BETWEEN 2000 AND 2500
